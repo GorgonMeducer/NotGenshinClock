52d6b03d4dd57181de268e8dac8276480b001918
Gabriel Wang <gabriel.wang@arm.com>
2023-06-30 05:27:23 +0100

    minor update

--- platform/Virtual_TFT_Port.c
+++ platform/Virtual_TFT_Port.c
@@ -97,7 +97,7 @@ static void monitor_sdl_init(void)
     SDL_SetEventFilter(quit_filter, NULL);
 
     window = SDL_CreateWindow(  
-                                "NotGenshin Clock(v0.5.0)"
+                                "NotGenshin Clock(v0.6.0)"
                                 "                                               - Created with Arm-2D v" 
                                 ARM_TO_STRING(ARM_2D_VERSION_MAJOR)
                                 "."
